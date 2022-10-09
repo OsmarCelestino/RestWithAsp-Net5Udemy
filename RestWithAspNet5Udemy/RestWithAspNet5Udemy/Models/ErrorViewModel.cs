b6a81b57ec59476057069fd5e81da4594b9b9304
Osmar Celestino <osmar10271@gmail.com>
2022-10-09 10:23:52 -0300

    primeiro commit

---
+++ RestWithAspNet5Udemy/RestWithAspNet5Udemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestWithAspNet5Udemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
