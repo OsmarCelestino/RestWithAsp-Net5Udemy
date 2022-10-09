42f3e79c0036463a44bd15254a3b440a81b0e779
Osmar Celestino <osmar10271@gmail.com>
2022-10-09 12:05:58 -0300

    Finalizando estrutura basica da calculadora

--- RestWithAspNet5Udemy/RestWithAspNet5Udemy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RestWithAspNet5Udemy.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
